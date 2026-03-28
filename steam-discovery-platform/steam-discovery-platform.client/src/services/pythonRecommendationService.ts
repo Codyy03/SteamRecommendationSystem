e609f4a214f15a6d316d4eab07138d85e3ce90d8
Codyy03 <sebastian998.pl@gmail.com>
2026-03-28 15:29:43 +0100

    fix pythonRecomentationService in frontend

--- steam-discovery-platform/steam-discovery-platform.client/src/services/pythonRecommendationService.ts
+++ steam-discovery-platform/steam-discovery-platform.client/src/services/pythonRecommendationService.ts
@@ -1,3 +1,7 @@
 export async function getPythonRecomentationGamesByName(gameName: string) {
     const response = await fetch(`https://localhost:7179/api/PythonRecommendation/pythonRecommendation?gameName=${gameName}`)
+
+    if (!response.ok) throw new Error("Error when downloading games");
+
+    return response.json();
 }
