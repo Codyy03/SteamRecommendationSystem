b3f4a07b3ab4bc6fbe26045020ecf25ae5eb6caf
Codyy03 <sebastian998.pl@gmail.com>
2026-04-04 21:34:12 +0200

    remove unnecessary declaration in NavBar component

--- steam-discovery-platform/steam-discovery-platform.client/src/components/Navbar.tsx
+++ steam-discovery-platform/steam-discovery-platform.client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/useAuth';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
